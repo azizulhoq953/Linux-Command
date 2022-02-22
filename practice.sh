67617f9f846a51d7eb306cd1d8da28886419800e
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-22 07:22:27 +0530

    modified

--- practice.sh
+++ practice.sh
@@ -1,10 +1,15 @@
-          Linux Git and Others Command test
+#           Linux Git and Others Command test
 
-git status,
- apt install git,
- echo {1..5},
-mkdir DevOps{1..3},
-mv Lecture_4 Lecture_4.txt,
-ls -li,
-link Lecture_05 LinkLecture06,
-rm -r *.txt
+# git status,
+#  apt install git,
+#  echo {1..5},
+# mkdir DevOps{1..3},
+# mv Lecture_4 Lecture_4.txt,
+# ls -li,
+# link Lecture_05 LinkLecture06,
+# rm -r *.txt
+
+
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"
